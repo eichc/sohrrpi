b5ddda6ae80f6f7145b29b5e17120bb3185b41bb
eichc <eich.cameron@gmail.com>
2024-03-12 17:17:06 -0400

    adding to sidebar

--- components/Sidebar/sidebar.tsx
+++ components/Sidebar/sidebar.tsx
@@ -1,14 +1,21 @@
+"use client";
 import styles from "./sidebar.module.css";
-import AddNew from "@/components/SidebarAddNew/sidebarAddNew";
+import SideBtn from "@/components/SidebarBtn/sidebarBtn";
 import List from "@/components/SidebarList/sidebarList";
+import AddCoursePopup from "@/components/AddCoursePopup/addCoursePopup";
+import { useState } from "react";
 
 export default function Sidebar() {
   let courses = ["CSCI 1100", "MATH 1010", "CSCI 1200"];
+  let [popupActive, setPopupActive] = useState(false);
 
   return (
     <div className={styles.sidecontainer}>
-      <List items={courses} />
-      <AddNew />
+      <div className={styles.sideflex}>
+        <List items={courses} />
+        <SideBtn symbol="+" text="Add New" isPopupActive={popupActive} onClick={() => setPopupActive(!popupActive)}/>
+        {popupActive ? <AddCoursePopup /> : <></>}
+      </div>
     </div>
   );
 }
--- components/SidebarAddNew/sidebarAddNew.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./sidebarAddNew.module.css";
-
-export default function SidebarAddNew() {
-  return (
-    <button className={styles.addnewbtn}>
-      Add New
-    </button>
-  );
-}

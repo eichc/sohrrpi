e242fc5554d6b3b2d3802073a8afe0b51ac72f5b
Emmanuelusman2004 <117528761+Emmanuelusman2004@users.noreply.github.com>
2024-03-15 16:18:49 -0400

    made the form to add a course
    Form to add a course is complete. The backend still needs to be implemented to add functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import styles from "./page.module.css";
 import Header from "@/components/Header/header";
 import Footer from "@/components/Footer/footer";
-import Login from "@/pages/Login/login"
+import Login from "@/pages/Login/login";
+import AddCourse from "@/pages/AddCourse/AddCourse";
+
 export default function Page() {
   return (
     <div>
       <div className={styles.page}>
         <Header />
       </div>
-      
       <Login />
+      <AddCourse />
 
       <div className={styles.footer}>
         <Footer />
